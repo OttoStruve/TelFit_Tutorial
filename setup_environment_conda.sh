d31c37087ea849434aa8f48d27dba523544a9e00
kgullikson <kevin.gullikson@gmail.com>
2016-01-03 12:45:46 -0600

    Added juptyer, ipython, and seaborn to conda packages.

--- setup_environment_conda.sh
+++ setup_environment_conda.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+
 # Create a new conda environment
-conda create -n telfit_tutorial python=2 numpy scipy matplotlib astropy cython requests
+conda create -n telfit_tutorial python=2 numpy scipy matplotlib astropy cython requests jupyter ipython-notebook seaborn
 
 source activate telfit_tutorial
 pip install -r requirements.txt
